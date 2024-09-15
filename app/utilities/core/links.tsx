90651c0d0ea35ef1298f67534d6dc0b24684d212
Dev Steve <wearypossum4770@yahoo.com>
2024-09-15 00:23:46 -0400

    add mising fles

---
+++ app/utilities/core/links.tsx
@@ -0,0 +1,66 @@
+import formStyle from "@/styles/forms.css";
+import mainStyle from "@/styles/main.css";
+import modalStyles from "@/styles/modals.css";
+import navigationStyle from "@/styles/navigation.css";
+import tableStyle from "@/styles/table.css";
+
+import { cssBundleHref } from "@remix-run/css-bundle";
+
+enum DocumentRelationship {
+	Alternate = "alternate",
+	Author = "author",
+	Dns = "dns-prefetch",
+	Help = "help",
+	Icon = "icon",
+	License = "license",
+	Next = "next",
+	Pingback = "pingback",
+	Preconnect = "preconnect",
+	Prefetch = "prefetch",
+	Preload = "preload",
+	Prerender = "prerender",
+	Prev = "prev",
+	Search = "search",
+	Stylesheet = "stylesheet",
+}
+
+enum CrossOriginRequest {
+	Anonymous = "anonymous",
+	Credentials = "use-credentials",
+}
+
+enum ReferrerPolicy {
+	Referrer = "no-referrer",
+	WhenDowngrade = "no-referrer-when-downgrade",
+	Origin = "origin",
+	CrossOrigin = "origin-when-cross-origin",
+	Unsafe = "unsafe-url",
+}
+
+type MediaLink = {
+	href: string;
+	rel: DocumentRelationship;
+	hreflang?: string;
+	crossOrigin?: CrossOriginRequest;
+	media?: string;
+	referrerPolicy?: ReferrerPolicy;
+	sizes?: string;
+	title?: string;
+	type?: string;
+};
+
+export const generateLinks = (): unknown =>
+	[
+		cssBundleHref ?? "",
+		mainStyle,
+		modalStyles,
+		tableStyle,
+		navigationStyle,
+		formStyle,
+	].reduce((a, href) =>
+		Object.assign(a, {
+			crossOrigin: CrossOriginRequest.Anonymous,
+			rel: "stylesheet",
+			href,
+		}),
+	);
---
+++ app/utilities/password-hashers/argon-hasher.ts
@@ -0,0 +1,41 @@
+import { hash, verify } from "@node-rs/argon2";
+
+import type {
+	BaseUser,
+	PrivateUserManager,
+	UnauthenticatedUser,
+} from "@/utilities/authentication/config";
+import { safePassword } from "@/utilities/password-hashers/config";
+
+export enum ArgonAlgorithm {
+	IndependentHash = 0,
+	DependentHash = 1,
+	HybridHash = 2,
+}
+
+export enum ArgonVersion {
+	version0x10 = 0,
+	/**
+	 * Default value
+	 * Version 19 (0x13 in hex, default)
+	 */
+	version0x13 = 1,
+}
+
+export const hashPassword = async ({
+	password,
+}: Pick<BaseUser, "password">): Promise<string | null> => {
+	"use server";
+	const pword = safePassword(password);
+	if (typeof pword === "string") return hash(pword.normalize("NFKC"));
+	return null;
+};
+
+export const verifyPassword = async ({
+	password,
+	hash,
+}: UnauthenticatedUser): Promise<boolean> => {
+	"use server";
+	const pword = safePassword(password);
+	return !pword ? false : verify(hash.normalize("NFKC"), pword);
+};
