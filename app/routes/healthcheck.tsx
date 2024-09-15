d0128bb2efdcd70dc7ba5e6e1da741df47bc30ef
Dev Steve <wearypossum4770@yahoo.com>
2024-09-15 18:57:42 -0400

    format using biome

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -6,60 +6,60 @@ export type HealthCheckRecord = Record<string, boolean>;
 export type HealthCheck = Promise<HealthCheckRecord>;
 export type HealthCheckList = HealthCheckRecord[];
 const outgoing = (url: string | URL) =>
-	new Request(new URL(url), {
-		method: "HEAD",
-	});
+  new Request(new URL(url), {
+    method: "HEAD",
+  });
 
 export const checkDatabaseConnection = () => {};
 
 export const checkNetworkConnection = ({ url }: Request, host: string) =>
-	new Promise((resolve, reject) =>
-		fetch(outgoing(new URL("/", `${new URL(url).protocol}//${host}`)))
-			.then(() => resolve({ checkNetworkConnection: true }))
-			.catch(() => reject({ checkNetworkConnection: false })),
-	);
+  new Promise((resolve, reject) =>
+    fetch(outgoing(new URL("/", `${new URL(url).protocol}//${host}`)))
+      .then(() => resolve({ checkNetworkConnection: true }))
+      .catch(() => reject({ checkNetworkConnection: false })),
+  );
 export const checkOutgoingConnection = async (): HealthCheck =>
-	new Promise((resolve, reject) =>
-		fetch(outgoing("https://example.com/"))
-			.then(() => resolve({ checkOutgoingConnection: true }))
-			.catch(() => reject({ checkOutgoingConnection: false })),
-	);
+  new Promise((resolve, reject) =>
+    fetch(outgoing("https://example.com/"))
+      .then(() => resolve({ checkOutgoingConnection: true }))
+      .catch(() => reject({ checkOutgoingConnection: false })),
+  );
 export const checkDatabaseQuery = async (): HealthCheck =>
-	new Promise((resolve, reject) =>
-		prisma.user
-			.count()
-			.then(() => resolve({ checkDatabaseQuery: true }))
-			.catch(() => reject({ checkDatabaseQuery: false })),
-	);
+  new Promise((resolve, reject) =>
+    prisma.user
+      .count()
+      .then(() => resolve({ checkDatabaseQuery: true }))
+      .catch(() => reject({ checkDatabaseQuery: false })),
+  );
 export const purposefulErrorResponse = () => {
-	throw new Response(
-		"ERROR: Purposeful response, nothing is wrong. You may proceede.",
-		{ status: 500 },
-	);
+  throw new Response(
+    "ERROR: Purposeful response, nothing is wrong. You may proceede.",
+    { status: 500 },
+  );
 };
 const getHostName = ({ headers }: Request) =>
-	headers.get("X-Forwarded-Host") ?? headers.get("host");
+  headers.get("X-Forwarded-Host") ?? headers.get("host");
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-	const host = getHostName(request);
+  const host = getHostName(request);
 
-	return new Promise((resolve, reject) => {
-		Promise.all([
-			checkDatabaseQuery(),
-			checkNetworkConnection(request, host ?? ""),
-			checkOutgoingConnection(),
-		])
-			.then(
-				(resp) =>
-					resp &&
-					resolve(
-						json(
-							resp.reduce((a: HealthCheckRecord, b) => Object.assign(a, b), {}),
-						),
-					),
-			)
-			.catch((error) => {
-				console.log("healthcheck ❌", { error });
-				return reject(new Response("ERROR", { status: 500 }));
-			});
-	});
+  return new Promise((resolve, reject) => {
+    Promise.all([
+      checkDatabaseQuery(),
+      checkNetworkConnection(request, host ?? ""),
+      checkOutgoingConnection(),
+    ])
+      .then(
+        (resp) =>
+          resp &&
+          resolve(
+            json(
+              resp.reduce((a: HealthCheckRecord, b) => Object.assign(a, b), {}),
+            ),
+          ),
+      )
+      .catch((error) => {
+        console.log("healthcheck ❌", { error });
+        return reject(new Response("ERROR", { status: 500 }));
+      });
+  });
 };
