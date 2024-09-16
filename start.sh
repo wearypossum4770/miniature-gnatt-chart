835b037fad581df0713b75ac7fefb0e21218f83f
Dev Steve <wearypossum4770@yahoo.com>
2024-09-16 09:44:28 -0400

    update start file

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-npx prisma migrate deploy
-npm run start
+bunx --bun  prisma migrate deploy
+bun run start
