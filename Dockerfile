5f3ba406c9d59f12202656a7b79436a13d169f8f
Dev Steve <wearypossum4770@yahoo.com>
2024-11-03 00:09:28 -0400

    correct docerfile

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ LABEL dev.fly.miniature-gnatt-chart-1d51=fullstack
 ENV NODE_ENV production
 
 # Install openssl for Prisma
-RUN ./scripts/setup-teardown.sh
+RUN touch ~/.bashrc ~/.profile ~/.zshrc  && chmod +x ~/.bashrc ~/.profile ~/.zshrc  && apt-get update && apt-get install -y openssl sqlite3 curl --no-install-recommends && export NVM_COLORS='cmgRY' && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && export NVM_DIR=$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm") \n[ -s "${XDG_CONFIG_HOME}/bash_completion" ] && \. "${XDG_CONFIG_HOME}/bash_completion" \n[ -s "${XDG_CONFIG_HOME}/nvm.sh" ] && bash -c "source  ~/.bashrc" && nvm install node && apt-get clean && rm -rf /var/lib/apt/lists/* 
 
 
 # Prepare for LiteFS installation
--- scripts/setup-teardown.sh
+++ scripts/setup-teardown.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-touch ~/.bashrc ~/.profile ~/.zshrc 
-chmod +x ~/.bashrc ~/.profile ~/.zshrc 
-apt-get update
-apt-get install -y openssl sqlite3 curl --no-install-recommends
-export NVM_COLORS='cmgRY'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm") \n[ -s "${XDG_CONFIG_HOME}/bash_completion" ] && \. "${XDG_CONFIG_HOME}/bash_completion" \n[ -s "${XDG_CONFIG_HOME}/nvm.sh" ]"
-bash -c "source  ~/.bashrc"
-nvm install node
-apt-get clean
-rm -rf /var/lib/apt/lists/* 
+touch ~/.bashrc ~/.profile ~/.zshrc  && chmod +x ~/.bashrc ~/.profile ~/.zshrc  && apt-get update && apt-get install -y openssl sqlite3 curl --no-install-recommends && export NVM_COLORS='cmgRY' && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && export NVM_DIR=$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm") \n[ -s "${XDG_CONFIG_HOME}/bash_completion" ] && \. "${XDG_CONFIG_HOME}/bash_completion" \n[ -s "${XDG_CONFIG_HOME}/nvm.sh" ] && bash -c "source  ~/.bashrc" && nvm install node && apt-get clean && rm -rf /var/lib/apt/lists/* 
 
+
+apt-get update && apt-get install -y openssl sqlite3 --no-install-recommends && apt-get clean && rm -rf /var/lib/apt/lists/*
